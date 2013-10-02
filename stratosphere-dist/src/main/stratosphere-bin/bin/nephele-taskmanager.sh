0532f000ce8996f5ac24813560288755d82a9bcf
Fabian Tschirschnitz <fabian.tschirschnitz@student.hpi.uni-potsdam.de>
2013-10-02 14:52:31 +0200

    unified classpaths in startup-scripts by replacing individual cp's with  from nephele-config.sh

--- stratosphere-dist/src/main/stratosphere-bin/bin/nephele-taskmanager.sh
+++ stratosphere-dist/src/main/stratosphere-bin/bin/nephele-taskmanager.sh
@@ -29,88 +29,6 @@ if [ "$NEPHELE_IDENT_STRING" = "" ]; then
 	NEPHELE_IDENT_STRING="$USER"
 fi
 
-# auxilliary function to construct a lightweight classpath for the
-# Nephele TaskManager
-constructTaskManagerClassPath() {
-
-	for jarfile in $NEPHELE_LIB_DIR/*.jar ; do
-
-		add=0
-
-		if [[ "$jarfile" =~ 'nephele-server' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-common' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-management' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-hdfs' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-s3' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-profiling' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'nephele-streaming' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'pact-common' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'pact-runtime' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-codec' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-httpclient' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-cli' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-logging' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-configuration' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'commons-lang' ]]; then
-                        add=1
-		elif [[ "$jarfile" =~ 'log4j' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'hadoop-core' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'httpcore' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'httpclient' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'aws-java-sdk' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'guava' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'sopremo-common' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'fastutil' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'javolution' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'kryo' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'reflectasm' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'minlog' ]]; then
-                        add=1
-		elif [[ "$jarfile" =~ 'asm' ]]; then
-			add=1
-		elif [[ "$jarfile" =~ 'objenesis' ]]; then
-                        add=1
-		fi
-
-		if [[ "$add" = "1" ]]; then
-			if [[ $NEPHELE_TM_CLASSPATH = "" ]]; then
-				NEPHELE_TM_CLASSPATH=$jarfile;
-			else
-				NEPHELE_TM_CLASSPATH=$NEPHELE_TM_CLASSPATH:$jarfile
-			fi
-		fi
-	done
-
-	echo $NEPHELE_TM_CLASSPATH
-}
-
-NEPHELE_TM_CLASSPATH=$(constructTaskManagerClassPath)
-
 log=$NEPHELE_LOG_DIR/nephele-$NEPHELE_IDENT_STRING-taskmanager-$HOSTNAME.log
 out=$NEPHELE_LOG_DIR/nephele-$NEPHELE_IDENT_STRING-taskmanager-$HOSTNAME.out
 pid=$NEPHELE_PID_DIR/nephele-$NEPHELE_IDENT_STRING-taskmanager.pid
@@ -134,7 +52,7 @@ case $STARTSTOP in
                 rotateLogFile $out
 
 		echo Starting Nephele task manager on host $HOSTNAME
-		$JAVA_HOME/bin/java $JVM_ARGS $NEPHELE_OPTS $log_setting -classpath $NEPHELE_TM_CLASSPATH eu.stratosphere.nephele.taskmanager.TaskManager -configDir $NEPHELE_CONF_DIR > "$out" 2>&1 < /dev/null &
+		$JAVA_HOME/bin/java $JVM_ARGS $NEPHELE_OPTS $log_setting -classpath $CLASSPATH eu.stratosphere.nephele.taskmanager.TaskManager -configDir $NEPHELE_CONF_DIR > "$out" 2>&1 < /dev/null &
 		echo $! > $pid
 	;;
 
